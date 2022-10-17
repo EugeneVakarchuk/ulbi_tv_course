2a170caeb03d82bc667de19e726b184f601db3df
EugeneVakarchuk <eugenevakarchuk@gmail.com>
2022-10-17 16:30:30 +0300

    Refactoring counter component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,14 @@
-import React, { useState } from 'react'
+import React from 'react'
+import Counter from './comp/Counter';
 
 function App() {
 
-  const [likes, setLikes] = useState(0);
-  const [value, setValue] = useState('Text in input area');
-
-  function increment() {
-    setLikes(likes + 1)
-  }
-
-  function decrement() {
-    setLikes(likes - 1)
-  }
-
   return (
     <div className="App">
-      <h1>{value}</h1>
-      <input
-        type='text'
-        value={value}
-        onChange={event => setValue(event.target.value)}
-      ></input>
-      <h1>{likes}</h1>
-      <button onClick={decrement}>Decrement</button>
-      <button onClick={increment}>Inctement</button>
+      <Counter />
+      <Counter />
+      <Counter />
+      <Counter />
     </div>
   );
 }
