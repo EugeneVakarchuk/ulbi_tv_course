3d570e2557900cd746b75dc0020e265d6df8b7d4
EugeneVakarchuk <eugenevakarchuk@gmail.com>
2022-10-17 19:10:00 +0300

    create controlled input

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import Counter from './comp/Counter';
 import PostList from './comp/PostList';
+import MyButton from './comp/ui/button/MyButton';
+import MyInput from './comp/ui/input/MyInput';
 import './styles/App.css';
 
 function App() {
@@ -10,23 +12,25 @@ function App() {
     { id: 3, title: 'JavaScript3', body: 'Description3' },
   ])
 
-  const [posts2, setPosts2] = useState([
-    { id: 1, title: 'Python', body: 'Description' },
-    { id: 2, title: 'Python2', body: 'Description2' },
-    { id: 3, title: 'Python3', body: 'Description3' },
-  ])
+  const [title, setTitle] = useState('')
 
-  const [posts3, setPosts3] = useState([
-    { id: 1, title: 'Java', body: 'Description' },
-    { id: 2, title: 'Java2', body: 'Description2' },
-    { id: 3, title: 'Java3', body: 'Description3' },
-  ])
+  const addNewPost = (e) => {
+    e.preventDefault();
+  }
 
   return (
     <div className="App">
+      <form>
+        <MyInput
+          onChange={e => setTitle(e.target.value)}
+          value={title}
+          type="text"
+          placeholder="Post name"
+        />
+        <MyInput type="text" placeholder="Post body" />
+        <MyButton onClick={addNewPost}>Create Post</MyButton>
+      </form>
       <PostList posts={posts} title="Posts for JS" />
-      <PostList posts={posts2} title="Posts for Python" />
-      <PostList posts={posts3} title="Posts for Java" />
     </div>
   );
 }
