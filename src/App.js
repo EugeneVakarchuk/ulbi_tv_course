76b9b92836fbcfc28c42f52989f6de4540c25fd8
EugeneVakarchuk <eugenevakarchuk@gmail.com>
2022-10-17 15:53:21 +0300

    create controlled Imput

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 function App() {
 
   const [likes, setLikes] = useState(0);
+  const [value, setValue] = useState('Text in input area');
 
   function increment() {
     setLikes(likes + 1)
@@ -14,6 +15,12 @@ function App() {
 
   return (
     <div className="App">
+      <h1>{value}</h1>
+      <input
+        type='text'
+        value={value}
+        onChange={event => setValue(event.target.value)}
+      ></input>
       <h1>{likes}</h1>
       <button onClick={decrement}>Decrement</button>
       <button onClick={increment}>Inctement</button>
